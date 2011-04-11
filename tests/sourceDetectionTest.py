7f307e10b7dacb302d92551eee66c72c6f9a5279
rowen <rowen@git.lsstcorp.org>
2011-04-11 22:45:39 +0000

    First cut. Can't run unit tests yet.

--- tests/sourceDetectionTest.py
+++ tests/sourceDetectionTest.py
@@ -37,6 +37,7 @@ import lsst.utils.tests as utilsTests
 import lsst.pex.policy as pexPolicy
 from lsst.pex.logging import Trace
 import lsst.afw.detection as afwDet
+import lsst.afw.geom as afwGeom
 import lsst.afw.image as afwImage
 import lsst.meas.utils.sourceDetection as sourceDetection
 
@@ -65,8 +66,8 @@ class DetectTestCase(unittest.TestCase):
         filename = os.path.join(eups.productDir("afwdata"),
                                 "CFHT", "D4", 
                                 "cal-53535-i-797722_1")
-        bbox = afwImage.BBox(afwImage.PointI(32,32), 512, 512)
-        exposure = afwImage.ExposureF(filename, 0,bbox)        
+        bbox = afwGeom.Box2I(afwGeom.Point2I(32, 32), afwGeom.Extent2I(512, 512))
+        exposure = afwImage.ExposureF(filename, 0, bbox, afwImage.LOCAL)        
         psf = sourceDetection.makePsf(self.psfPolicy)
        
         bck, bckSubExp = sourceDetection.estimateBackground(
--- tests/sourceMeasurementTest.py
+++ tests/sourceMeasurementTest.py
@@ -82,8 +82,8 @@ class SourceMeasurementTestCase(unittest.TestCase):
         filename = os.path.join(
             eups.productDir("afwdata"), "CFHT", "D4", "cal-53535-i-797722_1"
         )
-        bbox = afwImage.BBox(afwImage.PointI(32,32), 512, 512)
-        exposure =  afwImage.ExposureF(filename, 0, bbox)
+        bbox = afwGeom.Box2I(afwGeom.Point2I(32, 32), afwGeom.Extent2I(512, 512))
+        exposure =  afwImage.ExposureF(filename, 0, bbox, afwImage.LOCAL)
         psf = sourceDetection.makePsf(self.psfPolicy)
        
         bck, bckSubExp = sourceDetection.estimateBackground(
